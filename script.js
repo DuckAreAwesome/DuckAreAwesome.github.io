eab47f319ec2cfbb5ddf65901327c19c26055a8e
DuckAreAwesome <140848777+DuckAreAwesome@users.noreply.github.com>
2023-07-28 12:15:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function openLink() {
+  window.open("https://youtu.be/L31KYyjOwec");
+}
+
+function openLink_a() {
+window.open("https://www.youtube.com/watch?v=f1-2xRx2gnE");
+}
+
+function info() {
+  document.getElementById("textbox").scrollIntoView();
+}
+
+function game() {
+  document.getElementById("game").scrollIntoView();
+}
+
+function photos() {
+  document.getElementById("left_container").scrollIntoView();
+}
+
+function music() {
+  document.getElementById("musiccc").scrollIntoView();
+}
+
+
+
+
